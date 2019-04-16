b2fb6b72d2327d80040beeca08c029e823e59a7c
jakesboy2 <jcwaldrip@att.net>
2019-04-15 21:35:14 -0500

    got template rendering up and running

--- personalsite/urls.py
+++ personalsite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .Views import views
 
 urlpatterns = [
+    # Admin Panel
     path('admin/', admin.site.urls),
+    
+    # Home Page
+    path('', views.index, name='index'),
 ]
