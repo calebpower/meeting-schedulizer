ba5fa1cbff3ac7f431502b357b0b1ec04844e178
jakesboy2 <jcwaldrip@att.net>
2019-04-24 01:56:47 -0500

    post request working

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+    path('create_todo', views.CreateTodoItem.as_view(), name='create-todo')
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View
 
 # Create your views here.
 
 # Render the home page
 def index(request):
-    return render(request, 'home.html')
+    app_url = request.path
+    return render(request, 'home.html', {'app_url': app_url})
+
+# Handle creating a todo item
+class CreateTodoItem(View):
+    def post(self, request, *args, **kwargs):
+
+        # Print post request items for temporary test
+        # TODO save this data into DB
+        print(request.POST.get('todoTitle'))
+        print(request.POST.get('todoPriorityLevel'))
+        print(request.POST.get('todoDateDue'))
+        print(request.POST.get('todoDateDoing'))
+        print(request.POST.get('todoCatagory'))
+        print(request.POST.get('todoMessage'))
+        return redirect('index')
