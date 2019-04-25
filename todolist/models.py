b5a3a5b2ebdbcfd041026958a7ffb994265df1b2
jakesboy2 <jcwaldrip@att.net>
2019-04-24 19:55:58 -0500

    saving todo item to database successfully

--- todolist/models.py
+++ todolist/models.py
@@ -9,11 +9,11 @@ class TodoItem(models.Model):
     # A description of the todo item
     description = models.CharField(max_length=280, null=True)
     # The date the todo item was created (date.now)
-    date_created = models.DateTimeField('date created')
+    date_created = models.DateField('date created')
     # The date the todo item MUST be completed by
-    date_due = models.DateTimeField('date due')
+    date_due = models.DateField('date due')
     # The date the user plans on completing the todo item
-    date_doing = models.DateTimeField('date doing', null=True)
+    date_doing = models.DateField('date doing', null=True)
     # The priority level (defined by constants, 1-4)
     priority_level = models.IntegerField(default=0)
     # The catagory of the todo item (defined by constants, 1-4)
@@ -21,7 +21,19 @@ class TodoItem(models.Model):
     # If the todo item is marked as complete
     is_done = models.BooleanField(default=False)
     # If the todo item should always be shown regardless of dates
-    showAlways = models.BooleanField(default=False)
+    show_always = models.BooleanField(default=False)
 
     # model methods
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'description': self.description,
+            'dateCreated': self.date_created,
+            'dateDue': self.date_due,
+            'dateDoing': self.date_doing,
+            'priorityLevel': self.priority_level,
+            'catagory': self.catagory,
+            'isDone': self.is_done,
+            'showAlways': self.show_always,
+        }
     
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
+from . import models
+import datetime
 
 # Create your views here.
 
@@ -12,13 +14,27 @@ def index(request):
 # Handle creating a todo item
 class CreateTodoItem(View):
     def post(self, request, *args, **kwargs):
+        
+        # collect the info from the post request form
+        post_title = request.POST.get('todoTitle') if request.POST.get('todoTitle') else None
+        post_priority_level = request.POST.get('todoPriorityLevel') if request.POST.get('todoPriorityLevel') else None
+        post_date_due = request.POST.get('todoDateDue') if request.POST.get('todoDateDue') else None
+        post_date_doing = request.POST.get('todoDateDoing') if request.POST.get('todoDateDoing') else None
+        post_catagory = request.POST.get('todoCatagory') if request.POST.get('todoCatagory') else None
+        post_message = request.POST.get('todoMessage') if request.POST.get('todoMessage') else None
+        post_always_show = request.POST.get('alwaysShow') if request.POST.get('alwaysShow') else None
 
-        # Print post request items for temporary test
-        # TODO save this data into DB
-        print(request.POST.get('todoTitle'))
-        print(request.POST.get('todoPriorityLevel'))
-        print(request.POST.get('todoDateDue'))
-        print(request.POST.get('todoDateDoing'))
-        print(request.POST.get('todoCatagory'))
-        print(request.POST.get('todoMessage'))
+        # Save the data from the form into a todo object on the database
+        todo_item = models.TodoItem(
+            title=post_title,
+            description=post_message,
+            date_created=datetime.date.today(),
+            date_due=post_date_due,
+            date_doing=post_date_doing,
+            priority_level=post_priority_level,
+            catagory=post_catagory,
+            is_done=False,
+            show_always=True if post_always_show == 'on' else False
+        )
+        todo_item.save()
         return redirect('index')
