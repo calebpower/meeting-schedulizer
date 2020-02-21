a1d64c5346f6238c4cf99c9cb4cd697657fcbbb2
Caleb L. Power <caleb.power@axonibyte.com>
2020-02-20 18:44:38 -0600

    refactored javascript; added functionality to 'create project' button

--- static/js/style.js
+++ static/js/style.js
@@ -1,1 +1,30 @@
-console.log("you're pulling in style.js")
+$(document).ready(function() {
+
+  // do this only for the projects page
+  if($('#project-pane').length && $('#active-pane').length) {
+    
+    // make the columns resizable
+    Split(["#project-pane","#active-pane"], {
+      elementStyle: function (dimension, size, gutterSize) { 
+        $(window).trigger('resize'); // Optional
+        $("#projects").height($(window).height()
+            - $("#titular-nav").height()
+            - parseFloat($("#titular-nav").css('padding-top'))
+            - parseFloat($("#titular-nav").css('padding-bottom')));
+        return { 'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)' }
+      },
+      gutterStyle: function (dimension, gutterSize) { return {'flex-basis':  gutterSize + 'px'} },
+      sizes: [20,60,20],
+      minSize: 200,
+      gutterSize: 6,
+      cursor: 'col-resize'
+    });
+
+    $('#create-project-btn').on('click', function() {
+      $("#active-pane > div").hide();
+      $("#active-pane-create").show();
+    });
+
+  }
+
+});
