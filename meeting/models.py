c316ddd07eb9219f30b1567412cbd8fc0931de3b
Jacob Waldrip <jcwaldrip@att.net>
2020-02-23 16:05:52 -0600

    Completed authentication for creating and loggin in as a user (#5)
    Extended the user type to include a display name, completed login, and
    completed registration.

--- meeting/models.py
+++ meeting/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 ''' Denotes a single project. '''
 class Project(models.Model):
@@ -8,10 +11,25 @@ class Project(models.Model):
 ''' Denotes users related to a project. '''
 class ProjectUsers(models.Model):
     class UserProjectRole(models.IntegerChoices):
-        INVITED = 0,
-        ACTIVE = 1,
+        INVITED = 0
+        ACTIVE = 1
         OWNER = 2
     
     user_role = models.IntegerField(choices=UserProjectRole.choices)
     # TODO add variable to denote foreign key to User (waiting on Jacob)
+
+''' The user profile. Tied to a User account and created when a user is saved or created '''
+class Profile(models.Model):
+    objects = models.Manager()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=30, blank=False)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
     
--- meeting/urls.py
+++ meeting/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
-    path('projects', views.projects, name='projects')
+    path('projects', views.projects, name='projects'),
+    path('login', views.LoginProcess.as_view(), name="LoginProcess"),
+    path('register', views.RegisterProcess.as_view(), name="RegisterProcess")
 ]
