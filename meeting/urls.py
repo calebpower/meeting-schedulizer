5b6542cf533010f203c806b27ce52718000da0e7
Justin Hampton <60278651+jhampton6@users.noreply.github.com>
2020-02-27 01:13:44 -0600

    Availability (#13)
    * Add gitignore files for JetBrains IDE (/.idea)
    
    * Add Availability page with ability to view and select meetings (with mock meeting data) and front-end input functionality for first time-slot selection. Add bootstrap-material-datetimepicker files (includes momentjs)
    
    * Add TimeAvailability model with Integer field for meeting id
    
    * Add favicon file
    
    * Restructure availability to use separate html files for dryness;
    Modify urls to use a Class for get/post availability for a meeting;
    Implement functionality to save availability to database and get existing availability;
    (note to self: commit more often)
    
    * Indicate the meeting when adding availability
    
    * Warn user before leaving unsaved changes
    
    * Debug unsaved changes alert when saving
    
    * Badges on meetings to indicate number of time-slots
    
    * Minor UI improvements for active meeting indicator
    
    Co-authored-by: Caleb L. Power <cpower@uco.edu>

--- meeting/urls.py
+++ meeting/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('projects/<int:project_key>/meetings/create', views.createMeeting, name="createMeeting"),
     path('login', views.LoginProcess.as_view(), name="LoginProcess"),
     path('register', views.RegisterProcess.as_view(), name="RegisterProcess"),
+    path('availability', views.availability, name='Availability'),
+    path('availability/<int:meeting_id>', views.Availability.as_view(), name='meeting_availability')
 ]
