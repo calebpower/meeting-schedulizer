546cdde8e3e7e7f0871874e9a757067db054010c
ysakurai65 <hawk20605@yahoo.co.jp>
2020-03-14 15:17:07 -0500

    Clean up codes and add comments

--- meeting/views.py
+++ meeting/views.py
@@ -571,32 +571,22 @@ class Voting(View):
         return False
 
     def generateMeeting(self,teamMember):
+
         u = 0
         rq = []
         aq = []
         avaliaveTimeSlots = self.avaliaveTimeSlots
+
         while u < len(teamMember):
+
             k = -1
-            # Compare timeslots
             timeslots = teamMember[u].timeSlots
-            # print('Available Timeslot Length: ' + str(len(avaliaveTimeSlots)))
-            # print(teamMember[u].name + ': TimeSlots' +
-            #     ' Length: ' + str(len(timeslots)))
-            for i in range(len(avaliaveTimeSlots)):
-                # print('Results: ' + str(avaliaveTimeSlots[i].tStart) + '-' + str(
-                #     avaliaveTimeSlots[i].tEnd) + ' : ', end='')
-                for j in range(len(avaliaveTimeSlots[i].members)):
-                    print(str(avaliaveTimeSlots[i].members[j]) + ' ', end='')
-                print(' ')
+
             # Add to array for first iteration
             if len(avaliaveTimeSlots) == 0:
                 for s in range(len(timeslots)):
                     avaliaveTimeSlots.append(timeslots[s])
-                    avaliaveTimeSlots[len(avaliaveTimeSlots) -
-                                    1].members.append(teamMember[u].name)
-                    #  print(teamMember[u].name+' slots: ' +
-                    #     str(timeslots[s].tStart) + '-' + str(timeslots[s].tEnd))
-                    # print(teamMember[u].name + ' added!!!!')
+                    avaliaveTimeSlots[len(avaliaveTimeSlots) -1].members.append(teamMember[u].name)
             else:
 
                 for i in range(len(avaliaveTimeSlots)):
@@ -604,46 +594,33 @@ class Voting(View):
 
                         k = k + 1
                         ts1 = avaliaveTimeSlots[i]
-                        # print('i = ' + str(i))
                         ts2 = timeslots[j]
-                        # print('j = ' + str(j))
-                        # print('k = ' + str(k))
-                        # print('Slot' + str(k) + ': ')
-                        # print('Available slots: ' + str(avaliaveTimeSlots[i].tStart) + '-' + str(
-                        #     avaliaveTimeSlots[i].tEnd) + ' : ', end='')
-                        # for m in range(len(avaliaveTimeSlots[i].members)):
-                        #     print(avaliaveTimeSlots[i].members[m] + ' ', end='')
-                        # print(' ')
-                        # print(teamMember[u].name+' slots: ' +
-                        #     str(timeslots[j].tStart) + '-' + str(timeslots[j].tEnd))
+
                         if not self.checkMember(avaliaveTimeSlots[i].members, teamMember[u].name):
                             # Compare each time slot
                             if ts1.tStart > ts2.tEnd or ts2.tStart > ts1.tEnd:
-                                # no overlapping time
-                                # print('Case 5: No overlapping periods')
+
                                 if not self.checkSlot(aq, ts2):
                                     ts2.members.append(teamMember[u].name)
                                     aq.append(ts2)
-                                    # print(teamMember[u].name + ' added!!!!')
-                                    # print('Appended ts2')
+
                                 if not self.checkSlot(aq, ts1):
                                     aq.append(ts1)
-                                    # print('Appended ts1')
+          
                                 if j == 0:
                                     rq.append(avaliaveTimeSlots[i])
 
                             elif ts1.tStart == ts2.tStart and ts1.tEnd == ts2.tEnd:
-                                # same time slot
-                                # print('Case 0: This is same time slot')
-                                # print('Meeting time is avaliave from ' +
-                                #     str(ts1.tStart) + ' to ' + str(ts1.tEnd))
+
+                                # Same time slot
+
                                 ts = TestTimeSlot(ts1.tStart, ts1.tEnd, 1)
                                 for m in range(len(avaliaveTimeSlots[i].members)):
                                     ts.members.append(
                                         avaliaveTimeSlots[i].members[m])
                                 if not self.checkMember(ts.members, teamMember[u].name):
                                     ts.members.append(teamMember[u].name)
-                                    # print(teamMember[u].name + ' added!!!!')
+                                    
                                 if j == 0:
 
                                     rq.append(avaliaveTimeSlots[i])
@@ -654,31 +631,30 @@ class Voting(View):
 
                             elif ts1.tStart < ts2.tStart and ts1.tEnd < ts2.tEnd:
                                 if (ts1.tEnd - ts2.tStart) < self.MEETING_DURATION:
-                                    # print('Case 1A: Meeting time is ' + str(ts1.tEnd -
-                                    #                                         ts2.tStart) + ': Not enough time for meeting')
+
+                                    # Not enough time for meeting
+
                                     if not self.checkSlot(aq, ts2):
                                         ts2.members.append(teamMember[u].name)
                                         aq.append(ts2)
-                                        # print('Appended ts2')
+                             
                                     if not self.checkSlot(aq, ts1):
                                         aq.append(ts1)
-                                        # print('Appended ts1')
+                   
                                     if j == 0:
                                         rq.append(avaliaveTimeSlots[i])
 
                                 else:
+
                                     # Start time of B and end time of A is overlapping
-                                    # print(
-                                    #     'Case 1B: Start time of B and end time of A is overlapping')
-                                    # print('Meeting time is avaliave from ' +
-                                    #     str(ts2.tStart) + ' to ' + str(ts1.tEnd))
+
                                     ts = TestTimeSlot(ts2.tStart, ts1.tEnd, 0)
                                     for m in range(len(avaliaveTimeSlots[i].members)):
                                         ts.members.append(
                                             avaliaveTimeSlots[i].members[m])
                                     if not self.checkMember(ts.members, teamMember[u].name):
                                         ts.members.append(teamMember[u].name)
-                                        # print(teamMember[u].name + ' added!!!!')
+
                                     if j == 0:
                                         rq.append(avaliaveTimeSlots[i])
                                         aq.append(ts)
@@ -687,31 +663,30 @@ class Voting(View):
 
                             elif ts1.tStart > ts2.tStart and ts1.tEnd > ts2.tEnd:
                                 if (ts2.tEnd - ts1.tStart) < self.MEETING_DURATION:
-                                    # print('Case 2A: Meeting time is ' + str(ts2.tEnd -
-                                    #                                         ts1.tStart) + ': Not enough time for meeting')
+
+                                    #  Not enough time for meeting
+
                                     if not self.checkSlot(aq, ts2):
                                         ts2.members.append(teamMember[u].name)
                                         aq.append(ts2)
-                                        # print('Appended ts2')
+               
                                     if not self.checkSlot(aq, ts1):
                                         aq.append(ts1)
-                                        # print('Appended ts1')
+    
                                     if j == 0:
                                         rq.append(avaliaveTimeSlots[i])
 
                                 else:
+
                                     # Start time of A and End time of B is overlapping
-                                    # print(
-                                    #     'Case 2B: Start time of A and End time of B is overlapping')
-                                    # print('Meeting time is avaliave from ' +
-                                    #     str(ts1.tStart) + ' to ' + str(ts2.tEnd))
+
                                     ts = TestTimeSlot(ts1.tStart, ts2.tEnd, 1)
                                     for m in range(len(avaliaveTimeSlots[i].members)):
                                         ts.members.append(
                                             avaliaveTimeSlots[i].members[m])
                                     if not self.checkMember(ts.members, teamMember[u].name):
                                         ts.members.append(teamMember[u].name)
-                                        # print(teamMember[u].name + ' added!!!!')
+
                                     if j == 0:
 
                                         rq.append(avaliaveTimeSlots[i])
@@ -722,10 +697,7 @@ class Voting(View):
 
                             elif ts1.tStart >= ts2.tStart and ts1.tEnd <= ts2.tEnd:
                                 # Start time of A and End time of A is overlapping
-                                # print(
-                                #     'Case 3: Start time of A and End time of A is overlapping')
-                                # print('Meeting time is avaliave from ' +
-                                #     str(ts1.tStart) + ' to ' + str(ts1.tEnd))
+
                                 ts = TestTimeSlot(ts1.tStart, ts1.tEnd, 1)
                                 for m in range(len(avaliaveTimeSlots[i].members)):
                                     ts.members.append(
@@ -741,46 +713,39 @@ class Voting(View):
 
                             elif ts1.tStart <= ts2.tStart and ts1.tEnd >= ts2.tEnd:
                                 # Start time of B and End time of B is overlapping
-                                # print(
-                                #     'Case 4: Start time of B and End time of B is overlapping')
-                                # print('Meeting time is avaliave from ' +
-                                #     str(ts2.tStart) + ' to ' + str(ts2.tEnd))
+
                                 ts = TestTimeSlot(ts2.tStart, ts2.tEnd, 1)
                                 for m in range(len(avaliaveTimeSlots[i].members)):
                                     ts.members.append(
                                         avaliaveTimeSlots[i].members[m])
                                 if not self.checkMember(ts.members, teamMember[u].name):
                                     ts.members.append(teamMember[u].name)
-                                    # print(teamMember[u].name + ' added!!!!')
+                                    
                                 if j == 0:
                                     rq.append(avaliaveTimeSlots[i])
                                     aq.append(ts)
                                 else:
                                     aq.append(ts)
 
+                                    
+                            #Check if it's end of nested loop
+
                             li = len(avaliaveTimeSlots) - 1
                             lj = len(timeslots) - 1
-                            # print('j = ' + str(j) + ': len(timeslots) - 1 = ' + str(lj))
+
+                            # When iteration ends remove all objects in the remove queue
                             if len(rq) != 0 and li == i and lj == j:
                                 for r in range(len(rq)):
-                                    # print(str(rq[r].tStart) + '-' +
-                                    #     str(rq[r].tEnd) + ' is removed!!')
-                                    avaliaveTimeSlots.remove(rq[r])
-                                rq = []
+                                    if self.checkSlot(avaliaveTimeSlots, rq[r]):
+                                        avaliaveTimeSlots.remove(rq[r])
+                                rq = [] #empty remove queue
+                            # When iteration ends add all objects in the add queue
                             if len(aq) != 0 and li == i and lj == j:
                                 for a in range(len(aq)):
-                                    # if not checkMember(aq[a].members, teamMember[u].name):
-                                    # aq[a].members.append(teamMember[u].name)
-                                    # print(teamMember[u].name + ' added!!!!')
                                     avaliaveTimeSlots.append(aq[a])
-                                    # print(str(aq[a].tStart) + '-' +
-                                    #     str(aq[a].tEnd) + ' is appended!')
-                                aq = []
-                            # print('Count is ' + str(len(avaliaveTimeSlots)))
+                                aq = [] #empty add queue
 
             u += 1
-
-
 
     def post(self, request,):
         duplicateSlot = False
@@ -836,7 +801,6 @@ class Voting(View):
         value = request.POST.get('generate')
         print(value)
         if value == 'g':
-            print('generateMeeting is called')
             self.generateMeeting(self.teamMember)
 
         context = {
