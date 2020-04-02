3623968ae2cb0dd18a72afa429b3c22f10fde4f8
Minbo Sim <msim@uco.edu>
2020-04-02 02:46:41 -0500

    cursor, , navbar, background (not complete yet)

--- static/js/trail.js
+++ static/js/trail.js
@@ -1,20 +1,28 @@
-let pointer = document.querySelectorAll(".pointer")
+var cursor = document.querySelector("#cursor");
+window.addEventListener("mousemove", function(e) {
 
-pointer.forEach(function(e) {
-    mouseMove(e);
-})
-
-function mouseMove(pointer) {
-    document.addEventListener("mousemove", function(e) {
-        pointerPos(e, pointer)
+        cursor.style.top = e.pageY + "px";
+        cursor.style.left = e.pageX + "px";
     })
-}
+    /*
+    let pointer = document.querySelectorAll(".pointer")
 
-function pointerPos(e, pointer) {
-    mousePosX = e.x;
-    mousePosY = e.y;
-    pointer.style.left = mousePosX - (pointer.getBoundingClientRect().width * 0.5) + "px";
-    pointer.style.top = mousePosY - (pointer.getBoundingClientRect().height * 0.5) + "px";
+    pointer.forEach(function(e) {
+        mouseMove(e);
+    })
+
+    function mouseMove(pointer) {
+        document.addEventListener("mousemove", function(e) {
+            pointerPos(e, pointer)
+        })
+    }
+
+    function pointerPos(e, pointer) {
+        mousePosX = e.x;
+        mousePosY = e.y;
+        pointer.style.left = mousePosX - (pointer.getBoundingClientRect().width * 0.5) + "px";
+        pointer.style.top = mousePosY - (pointer.getBoundingClientRect().height * 0.5) + "px";
 
 
-}
+    }
+    */
