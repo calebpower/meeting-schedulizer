9b600cc48fcf72ad8ec90398bc3a8425b278409b
Jacob Waldrip <jcwaldrip@att.net>
2020-04-02 16:59:44 -0500

    completed logout (#53)
    Co-authored-by: Caleb L. Power <cpower@uco.edu>

--- meeting/urls.py
+++ meeting/urls.py
@@ -11,30 +11,35 @@ urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
     
-    #Availability
+    # Availability
     path('availability', availability.availability, name='Availability'),
     path('availability/<int:meeting_id>', availability.Availability.as_view(), name='meeting_availability'),
     path('availability/<int:meeting_id>/delete', availability.AvailabilityDelete.as_view(), name='AvailabilityDelete'),
     
-    #Notification
+    # Notification
     path('notification_demo', notifications.notification_demo, name='notification_demo'),
     path('notifications', notifications.NotificationProcess.as_view(), name='NotificationProcess'),
     
-    #User
+    # User
     path('login', authentication.LoginProcess.as_view(), name="LoginProcess"),
     path('profile', authentication.ProfilePage.as_view(), name="ProfilePage"),
     path('register', authentication.RegisterProcess.as_view(), name="RegisterProcess"),
     
-    #Project
+    # Project
     path('projects', projects.projects, name='projects'),
     path('projects/create', projects.ProjectCreationProcess.as_view(), name='ProjectCreationProcess'),
     path('projects/<int:project_key>', projects.ProjectViewProcess.as_view(), name="ProjectViewProcess"),
     path('projects/<int:project_key>/edit', projects.ProjectModificationProcess.as_view(), name="ProjectModificationProcess"),
     
-    #Meeting
+    # Meeting
     path('projects/<int:project_key>/meetings/create', meetings.MeetingView.as_view(), name="MeetingView"),
     path('projects/<int:meeting_key>/meetings/view', meetings.MeetingEdit.as_view(), name="MeetingEdit"),
 
-    #Voting
+    # Voting
     path('voting', voting.Voting.as_view(), name='Voting'),
+  
+    # Authentication
+    path('register', authentication.RegisterProcess.as_view(), name="RegisterProcess"),
+    path('voting', voting.Voting.as_view(), name='Voting'),
+    path('logout', authentication.LogoutPage.as_view(), name="LogoutPage")
 ]
